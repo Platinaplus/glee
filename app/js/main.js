858b7560c00aff3724313980893028e3fb0091c4
Platina <prokofievan@yahoo.com>
2020-11-22 17:49:51 +0300

    Filter added

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,37 @@
-$(function(){
+//SLIDER TOP HOMEPAGE---------------------------------
 
-  $('.top-slider__inner').slick({
+$(function () {
+  $(".top-slider__inner").slick({
     dots: true,
-    arrows:false,
+    arrows: false,
     fade: true,
     autoplay: true,
-    autoplaySpead: 3000
-  })
+    autoplaySpead: 3000,
+  });
 
+  //FILTER PRODUCTS HOMEPAGE----------------------------
 
+  var mix1 = mixitup(".new-design__cards", {
+    selectors: {
+      control: ".new-design__menu-item",
+    },
+    callbacks: {
+      onMixStart: function (state, futureState) {
+        $(".new-design__menu-item").removeClass("on");
+        $(event.target).addClass("on");
+      },
+    },
+  });
 
-
+  var mix2 = mixitup(".products__cards", {
+    selectors: {
+      control: ".products__menu-item",
+    },
+    callbacks: {
+      onMixStart: function (state, futureState) {
+        $(".products__menu-item").removeClass("on");
+        $(event.target).addClass("on");
+      },
+    },
+  });
 });
