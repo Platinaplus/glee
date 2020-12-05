5ed9646d9f9209f2f807de6c5277af058a0b2617
Platina <prokofievan@yahoo.com>
2020-12-05 11:25:40 +0300

    catalog added

--- app/js/main.js
+++ app/js/main.js
@@ -11,27 +11,143 @@ $(function () {
 
   //FILTER PRODUCTS HOMEPAGE----------------------------
 
-  var mix1 = mixitup(".new-design__cards", {
-    selectors: {
-      control: ".new-design__control",
-    },
-    callbacks: {
-      onMixStart: function (state, futureState) {
-        $(".new-design__control").removeClass("on");
-        $(event.target).addClass("on");
+  if ($(".new-design__cards").length) {
+    var mix1 = mixitup(".new-design__cards", {
+      selectors: {
+        control: ".new-design__control",
       },
+      callbacks: {
+        onMixStart: function (state, futureState) {
+          $(".new-design__control").removeClass("on");
+          $(event.target).addClass("on");
+        },
+      },
+    });
+  }
+
+  if ($(".products__cards").length) {
+    var mix2 = mixitup(".products__cards", {
+      selectors: {
+        control: ".products__control",
+      },
+      callbacks: {
+        onMixStart: function (state, futureState) {
+          $(".products__control").removeClass("on");
+          $(event.target).addClass("on");
+        },
+      },
+    });
+  }
+
+  // RANGE PRICE-----------------------------------------
+  $(".filter__input").ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onChange: function (data) {
+      $(".filter__from").text(data.from);
+      $(".filter__to").text(data.to);
     },
+    onStart: function (data) {
+      $(".filter__from").text(data.from);
+      $(".filter__to").text(data.to);
+    }
+  });
+  
+  // RAITING
+
+  $(".filter-recent__stars").rateYo({
+    starWidth: "10px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+
   });
 
-  var mix2 = mixitup(".products__cards", {
-    selectors: {
-      control: ".products__control",
-    },
-    callbacks: {
-      onMixStart: function (state, futureState) {
-        $(".products__control").removeClass("on");
-        $(event.target).addClass("on");
-      },
-    },
+  $(".products__stars").rateYo({
+    starWidth: "18px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "12px"
+
   });
+
+  $('.buttons__view').on('click', function () {
+    $('.buttons__view').removeClass('buttons__view--active');
+    $(this).addClass('buttons__view--active');
+  });
+  
+  $('.products__button').on('click', function () {
+    $(this).toggleClass('products__button--active');
+  });
+
+  $('.buttons__view--list').on('click', function () {
+    $('.catalog__inner').addClass('catalog__inner--list')
+    $('.products__card').addClass('products__card--list')
+    $('.catalog__products').addClass('catalog__products--list')
+  });
+
+  $('.buttons__view--grid').on('click', function () {
+    $('.catalog__inner').removeClass('catalog__inner--list')
+    $('.products__card').removeClass('products__card--list')
+    $('.catalog__products').removeClass('catalog__products--list')
+  });
+
+
+  //GOODS IN CART AND IN WISHLIST
+
+  const addTo = function (selector) {
+    let value = $(selector).attr('data-value');
+    value++;
+    $(selector).attr('data-value', value).text(value)
+  };
+
+  const anime = function(selector, modalSelector, text) {
+    $(selector)
+    .clone()
+    .css({
+      'position': 'absolute',
+      'z-index': '999'
+    })
+    .appendTo($(selector).parent())
+    .animate({
+      top: '-500px',
+      left: '500px',
+      opacity:0
+    },2000, function() {
+      $(this).remove();})
+      $(modalSelector).html(text + '<span>+</span>').show(1500).hide(600)
+      // .animate({
+      //   opacity:'1'
+      // });
+      $(modalSelector +' span').on('click', function () {
+        $(modalSelector).hide(300);
+      });
+      // setInterval(function() {
+      //   $(modalSelector).hide()
+      // }, 3500);
+    
+  };
+
+  $('.products__button--cart, .products__icon--cart').on('click', function (e) {
+    e.preventDefault();
+    addTo('.user-nav__num--cart');
+    anime('.products__icon--cart', '.user-nav__modal', 'Added to cart !');
+  });
+
+  $('.products__button--love, .products__icon--love').on('click', function (e) {
+    e.preventDefault();
+    addTo('.user-nav__num--love');
+    anime('.products__icon--love', '.user-nav__modal', 'Added to wishlist !');
+  });
+
+
+
+  
+
+
+ 
+
 });
+
+
